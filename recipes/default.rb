ebe01e031d4460638e080ae4fa4bfaa5c29a54a9
Chris Roberts <chrisroberts.code@gmail.com>
2012-04-10 10:30:58 -0700

    Only setup if attributes dictate

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,43 +1,8 @@
-# Basic setups
-#
-pkgs = value_for_platform(
-  %w(redhat centos fedora scientific) => {
-    %w(5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.7 5.8) => %w(fcgi-perl),
-    "default" => %w(perl-FCGI perl-FCGI-ProcManager)
-  },
-  %w(debian ubuntu) => {
-    "default" => %w(libfcgi-perl libfcgi-procmanager-perl)
-  },
-  "default" => %w(libfcgi-perl libfcgi-procmanager-perl)
-)
 
-pkgs.each do |package_name|
-  package package_name
+# By default, nothing is setup. This allows cookbooks dependent on
+# simplecgi to define how they would like the setup to occur without
+# relying on attributes being set directly within nginx_simplecgi
+
+if(node[:nginx_simplecgi][:cgi] || node[:nginx_simplecgi][:php])
+  include_recipe 'nginx_simplecgi::setup'
 end
-
-if(pkgs.include?('fcgi-perl'))
-  include_recipe 'perl'
-  cpan_module 'FCGI::ProcManager'
-end
-
-directory node[:nginx_simplecgi][:dispatcher_directory] do
-  action :create
-  recursive true
-  owner node[:nginx][:user]
-  group node[:nginx][:group] || node[:nginx][:user]
-end
-
-# Setup our dispatchers
-include_recipe 'nginx_simplecgi::cgi' if node[:nginx_simplecgi][:cgi]
-include_recipe 'nginx_simplecgi::php' if node[:nginx_simplecgi][:php]
-
-# Setup our init
-
-case node[:nginx_simplecgi][:init_type].to_sym
-when :upstart
-  include_recipe 'nginx_simplecgi::cgi-upstart' if node[:nginx_simplecgi][:cgi]
-  include_recipe 'nginx_simplecgi::php-upstart' if node[:nginx_simplecgi][:php]
-else
-  raise "Not Implemented: #{node[:nginx_simplecgi][:init_type]}"
-end
-
