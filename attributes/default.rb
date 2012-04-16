0ece30b87525a024a00eede8f45d70ae5490cbeb
Chris Roberts <chrisroberts.code@gmail.com>
2012-04-16 11:47:07 -0700

    Properly use default when setting attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,13 @@
-node[:nginx_simplecgi] = Mash.new
+default[:nginx_simplecgi] = Mash.new
 # Enable CGI dispatcher
-node[:nginx_simplecgi][:cgi] = false
+default[:nginx_simplecgi][:cgi] = false
 # Enable PHP dispatcher
-node[:nginx_simplecgi][:php] = false
+default[:nginx_simplecgi][:php] = false
 # Directory to contain dispatcher socket and pid file
-node[:nginx_simplecgi][:dispatcher_directory] = '/var/run/nginx'
+default[:nginx_simplecgi][:dispatcher_directory] = '/var/run/nginx'
 # Number of dispatcher process to handle requests
-node[:nginx_simplecgi][:dispatcher_processes] = 4
+default[:nginx_simplecgi][:dispatcher_processes] = 4
 # Location of PHP CGI executable
-node[:nginx_simplecgi][:php_cgi_bin] = '/usr/bin/php-cgi'
+default[:nginx_simplecgi][:php_cgi_bin] = '/usr/bin/php-cgi'
 # Type of init (:upstart, :runit, :bluepill, :monit)
-node[:nginx_simplecgi][:init_type] = :upstart
+default[:nginx_simplecgi][:init_type] = :upstart
