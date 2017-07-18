0c14a5f0ba155780f1285eaa05ab5b963964a1b5
Tim Smith <tsmith@chef.io>
2017-07-18 10:36:20 -0700

    Use a SPDX license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'nginx_simplecgi'
 maintainer       'Tim Smith'
 maintainer_email 'tsmith@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Provides SimpleCGI for NGINX'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.3'
